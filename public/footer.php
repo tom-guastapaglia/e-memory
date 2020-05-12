599ff85a3a0c216ce14449f72ede2c5dec835db6
Tom-Guastapaglia <tomguasta@yahoo.fr>
2020-05-12 10:02:13 +0200

    Test 12/05/2020

---
+++ public/footer.php
@@ -0,0 +1,19 @@
+<footer>
+    <hr>
+    <div class="container_footer">
+        <div class="box_one">
+            <p>Design by E-memory | Created by E-memory</p>
+            <p>E-MEMORY © 2020 </p>
+        </div>
+        <div class="box_two">
+            <ul id="footer_ul">
+                <li><a href="#">CGU</a></li>
+                <li><a href="#">Mentions légales</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
+</body>
+
+</html>
---
+++ public/index.php
@@ -0,0 +1,90 @@
+<?php include("header.php") ?>
+<div class="container_header">
+    <div class="box_one">
+        <ul id="menu_ul">
+            <li><a href="index.php">Accueil</a></li>
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Les thèmes</a>
+                <div class="dropdown-content">
+                    <a href="#">Mariage</a>
+                    <a href="#">Anniversaire</a>
+                    <a href="#">Amour</a>
+                    <a href="#">Famille</a>
+                    <a href="#">Naissance</a>
+                    <a href="#">Album vierge</a>
+                </div>
+            </li>
+            <li><a href="#">Contact</a></li>
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Mon compte</a>
+                <div class="dropdown-content">
+                    <a href="#">Mes albums</a>
+                    <a href="#">Créer un album</a>
+                    <a href="#">Mes photos</a>
+                    <a href="#">Mes informations</a>
+                    <a href="#">Se déconnecter</a>
+                </div>
+            </li>
+        </ul>
+        <hr class="header_hr">
+    </div>
+    <div class="bow-two">
+        <h1 id="logo_title">E-MEMORY</h1>
+    </div>
+</div>
+
+<main>
+    <div class="container_main">
+        <div class="box_one_content">
+            <h2>Vos souvenirs, 100% numériques</h2>
+            <h3>Téléchargez vos plus belles photos</h3>
+            <p>Depuis des années, ces photos n’attendent que d’être placésdans l’album que vous rêvez de mettre en oeuvre. Vous voyez celles dont je parle… Le problème c’est que vous ne pourrez plus modifier album une fois imprimé par votre site favoris vous l’ayant facturé une fortune… C’est pourquoi, vous devez dès maintenant créer un compte chez E-MEMORY et enfin développer l’album de vos rêves. Modifiable à l’infini. Des centaines de thèmes pour vos évènements photos. Un partage facile en un clique sur la toile et tout vos résaux sociaux.</p>
+        </div>
+        <div class="bow_two_content">
+            <img class="img_index" src="/assets/images/Illustration_photo.png" alt="Illustration photo">
+        </div>
+    </div>
+
+    <div class="container_main">
+        <div class="box_one_content">
+            <img class="img_index" src="/assets/images/Illustration_loading.png" alt="Illustration envoi">
+        </div>
+        <div class="bow_two_content">
+            <h2>Un dossier, et c’est prêt</h2>
+            <h3>Envoyer votre dossier de photos et vidéos</h3>
+            <p>Pour mettre en ligne votre album, vous n’avez qu’à organiser vos photos dans un album dédié. Une fois que ce dernier est prêt, glissez le dans votre projet pour ajouter les photos à l’album. Vous pouvez à tout moment supprimer, rajouter, ou modifier une photo. Après si peu de travail, vous pouvez choisir votre thème…</p>
+        </div>
+    </div>
+
+    <div class="container_main">
+        <div class="box_one_content">
+            <h2>Maintenant, on design !</h2>
+            <h3>Choix du thème et personnalisation</h3>
+            <p>Une fois les premières photos ajoutées, vous pouvez choisir entre différents thèmes d’album :</p>
+            <ul>
+                <li>Mariage</li></br>
+                <li>Anniversaire</li></br>
+                <li>Amour</li></br>
+                <li>Naissance</li></br>
+                <li>Famille : ce thème est plus conséquent. En effet, il permet de regrouper plusieurs albums entre eux pour créer un vrai souvenir pour un grande famille.</li></br>
+            </ul>
+        </div>
+        <div class="bow_two_content">
+            <img class="img_index" src="/assets/images/Illustration_album.png" alt="Illustration photo">
+        </div>
+    </div>
+
+    <div class="container_main">
+        <div class="box_one_content">
+            <img class="img_index" src="/assets/images/Illustration_partage.png" alt="Illustration envoi">
+        </div>
+        <div class="bow_two_content">
+            <h2>Vous avez fini ?</h2>
+            <h3>Alors partagez votre album !</h3>
+            <p>Votre album sera disponible sur un site internet dédié avec un nom que vous pouvez choisir. Il est de plus disponible hors-ligne dans votre poche grâce à notre application. Vous pouvez donc partagez sur tous les réseaux sociaux votre site web souvenir. Pour protéger vos souvenirs, vous pouvez également choisir de verrouiller l’entré de l’album avec un mot de passe. Votre album reste modifiable à l’infini grâce à votre interface de création sur votre compte E-Memory.</p>
+        </div>
+    </div>
+
+</main>
+
+<?php include("footer.php") ?>
